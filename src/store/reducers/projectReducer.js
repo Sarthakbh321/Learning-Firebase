4eb7e73f4b48ff89e10eeb0497ef78e6ff7a2396
Sarthak <sarthakbh321@gmail.com>
2021-02-17 23:12:09 +0530

    project reducer fix

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -6,6 +6,7 @@ const projectReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case "CREATE_PROJECT": {
 			return {
+				...state,
 				projects: [...state.projects, action.payload],
 			};
 		}
