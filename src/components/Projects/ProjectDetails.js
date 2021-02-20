49105e2e6e4ae5a540234444a2b88c5f8bf4b07e
Sarthak <sarthakbh321@gmail.com>
2021-02-20 21:22:54 +0530

    get single project from firebase

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "../../utils/firebase";
 
 function ProjectDetails(props) {
+	const [project, setProject] = useState({});
 	const id = props.match.params.id;
+
+	const getProject = async () => {
+		const getDoc = await firebase
+			.firestore()
+			.collection("projects")
+			.doc(id)
+			.get();
+
+		console.log(getDoc.data());
+		setProject(getDoc.data());
+	};
+
+	useEffect(() => {
+		getProject();
+	}, []);
 
 	return (
 		<div className="container section project-details">
 			<div className="card z-depth-0">
 				<div className="card-content">
-					<span className="card-title">Project Title - {id}</span>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Fugiat ipsa repellendus itaque soluta adipisci repellat
-						quis cupiditate dignissimos odio, officia maxime
-						ratione. Asperiores placeat suscipit et repellendus
-						temporibus porro fugiat!
-					</p>
+					<span className="card-title">{project.title}</span>
+					<p>{project.content}</p>
 				</div>
 				<div className="card-action grey lighten-4 grey-text">
-					<div>Posted by Sarthak</div>
+					<div>Posted by {project.author}</div>
 					<div>2nd September, 2AM</div>
 				</div>
 			</div>
--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import ProjectSummary from "./ProjectSummary";
 
 function ProjectList({ projects }) {
+	const history = useHistory();
+
+	const handleClick = (id) => {
+		history.push(`/project/${id}`);
+	};
+
 	return (
 		<div className="project-list section">
 			{projects.map((project, i) => (
-				<ProjectSummary key={project.id} details={project} />
+				<div
+					key={project.id}
+					onClick={() => handleClick(project.id)}
+					className="project-tile"
+				>
+					<ProjectSummary details={project} />
+				</div>
 			))}
 		</div>
 	);
